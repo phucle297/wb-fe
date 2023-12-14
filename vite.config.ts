604c2be38ce9cecb3286117399bf8f8f2715adff
Phúc Lê <somdp2907@gmail.com>
2023-12-14 23:56:44 +0700

    test: ci

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,12 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
+  // @ts-ignore
+  test: {
+    include: ["**/*.spec.[tj]s", "**/*.spec.[tj]sx"],
+    exclude: ["**/node_modules/**", "**/dist/**"],
+    testTimeout: 20000,
+  },
   plugins: [
     react(),
     svgr({
