95cfb276a20a2c7edc6303c37f196c0af1ba4400
Phúc Lê <somdp2907@gmail.com>
2023-12-17 18:05:18 +0700

    chore: install dependencies, update logo and title

--- src/pages/(routes)/home/index.tsx
+++ src/pages/(routes)/home/index.tsx
@@ -1,10 +1,21 @@
+import { Button, Modal } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+
 import styles from "./index.module.scss";
 
 const Home = () => {
+  const [opened, { open, close }] = useDisclosure(false);
+
   return (
     <div className={styles.wrapper}>
       <h1>Home</h1>
       <button>ChangeTheme</button>
+
+      <Modal opened={opened} title="Authentication" onClose={close}>
+        {/* Modal content */}
+      </Modal>
+
+      <Button onClick={open}>Open modal</Button>
     </div>
   );
 };
